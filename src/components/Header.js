48e2fe78b145589cf06cca85c45d6e4d51cc79f0
james-horton <email@example.com>
2020-08-29 18:22:39 -0400

    adjusted logo width for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import styles from './Header.module.css';
 import avatar from '../images/moo.png';
-import titleLogo from '../images/moovid-logo.png'
+import logo from '../images/moovid-logo.png'
 
 const Header = () => {
     return (
         <div className={styles.header}>                 
             <img className={`${styles.headerlogo} center-image`} src={avatar} alt="avatar" />
-            <img className="center-image" src={titleLogo} alt="logo" />
+            <img className={`${styles.titlelogo} center-image`} src={logo} alt="logo" />
         </div>
     );
 }
